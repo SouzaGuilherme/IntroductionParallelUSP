5c16bd921674574649c629ca510dfdac07a9d1db
SouzaGuilherme <gdsdsilva@inf.ufpel.edu>
2019-04-22 17:36:28 -0300

    Funcionando porem com a variavel word_search sendo passada adiante mais nao utilizada

--- que.c
+++ que.c
@@ -1,3 +1,4 @@
+#include<regex.h>
 #include<dirent.h>
 #include<pthread.h>
 #include<stdio.h>
@@ -24,28 +25,14 @@ typedef struct works{
 }works_threads;
 
 pthread_mutex_t mutex;
+regex_t reg;
 
-int boyerMoore(byte a[], int m, byte b[], int n){
-    int ult[256]; // o alfabeto é 0..255
-    // pré-processamento da palavra a
-    for (int f = 0; f < 256; ++f) ult[f] = 0;
-    for (int i = 1; i <=  m; ++i) ult[a[i]] = i;
-    // busca da palavra a no texto b
-    int ocorre = 0;
-    int k = m;
-    while (k <= n) {
-        // a[1..m] casa com b[k-m+1..k]?
-        int i = m, j = k;
-        while (i >= 1 && a[i] == b[j]) 
-            --i, --j;   
-        if (i < 1) ++ocorre;
-        if (k == n) k += 1;
-        else k += m - ult[b[k+1]] + 1;
-    }
-    if (ocorre > 0)
+int search_reg(byte a[], int m, byte b[], int n){   
+    if ((regexec(&reg, (char*)b, 0, (regmatch_t *)NULL, 0)) == 0){
         return 1;
-    else
+    }else{
         return 0;
+    }
 };
 
 void greep(arq_dir *content_file){
@@ -63,7 +50,7 @@ void greep(arq_dir *content_file){
 		int g=-1, i=0;
         while(fgets(linha, sizeof(linha), arq) != NULL){
 			g++;
-            number_ocorr = boyerMoore((byte*)teste, (strlen(teste)-1), (byte*)linha, (strlen(linha)-1));
+            number_ocorr = search_reg((byte*)teste, (strlen(teste)-1), (byte*)linha, (strlen(linha)-1));
 		    if (number_ocorr == 1){
                 content_file->lane[i] = g;
                 i++;
@@ -76,9 +63,6 @@ void greep(arq_dir *content_file){
 void main_threads(void *arg){
     works_threads *task = (works_threads*)arg;
     arq_dir *pointer = task->pointer_directory;
-    if (task->end == 0 && task->start == 0)
-        greep(pointer);        
-    
     for(int i = task->start; i < task->end; i++){
         greep(pointer);
 		pointer++;
@@ -89,28 +73,31 @@ int cria_thread(arq_dir *content_file, int number_thread){
     pthread_t threads[number_thread];
 	works_threads *task = malloc(number_thread*sizeof(works_threads));
     int division_work = (content_file->position)%(number_thread);
+    int offset = -1;
+    int pos = content_file->position;
 	for(int i = 0; i < number_thread; i++){
-		int work_size = (content_file->position)/(number_thread);
+		int work_size = (pos / number_thread);
 		if (i < division_work) 
 			work_size += 1;
-		int start = i * work_size;
+        int start = -1;
+        if (offset == -1) {
+            offset = 0;
+            start = 0;
+        } else {
+            start = offset;
+        }
 		task[i].start = start;
-		int end = (i+1) * work_size;
+		int end = offset + (work_size);
 		task[i].end = end;
 		task[i].pointer_directory = content_file;
-        
+        offset = end;
+
 		int rc = pthread_create(&threads[i], NULL, (void*)main_threads, (void*)&task[i]);
       	if (rc){
         	printf("ERROR; return code from pthread_create() is %d\n", rc);
          	return -1;
       	}
-        if (start == 0 && end == 0){
-            content_file++;
-        }else{
-            for (int p = 0; p < end+1; p++){
-                content_file++;
-            }
-        }
+        content_file += (end-start);
 	}
 
     for (int i = 0; i < number_thread; ++i) {
@@ -148,22 +135,19 @@ int list_directory(char *directory, arq_dir *content_file, char *word_search, in
                 exit(1);
             }
             printf("PASSA?");
-            // Volto
-            // Printo (isso tem que ser uma secao critica)
+
             pthread_mutex_lock(&mutex);
             for(int h = 0; h < content_file->position; h++){
                 if ((content_file+h)->lane[0] != -1){
-                    printf("%s\n", (content_file+h)->dir_name);
                     int k=0;
                     while ((content_file+h)->lane[k] != -1){
-                        printf("Number lane: %d\n", (content_file+h)->lane[k]);
+                        printf("%s:", (content_file+h)->dir_name);
+                        printf("%d\n", (content_file+h)->lane[k]);
                         k++;
                         if ((content_file+h)->lane[k] == 0){
                             break;
                         }
                     }
-                    printf("Type: %d\n", (content_file+h)->dir_type);
-                    printf("%d\n\n", h);
                 }
             }
             content_file->position = 0;
@@ -191,59 +175,55 @@ int list_directory(char *directory, arq_dir *content_file, char *word_search, in
 
 int main(int argc, const char *argv[]){
     if (argc < 4) {
-        printf("Comando invalido\nComando:\n\n    ./pgrep <diretorio> <numero de threads> <palavra a ser buscada>\n\n");
+        printf("Comando invalido\nComando:\n\n    ./pgrep <numero de threads> <palavra a ser buscada> <diretorio>\n\n");
         exit(1);
     }
-    int number_thread = atoi(argv[2]);
+    int number_thread = atoi(argv[1]);
     char directory[word_max];
     char word_search[word_max];
-    strcpy(directory, argv[1]);
-    strcpy(word_search, argv[3]);
+    strcpy(directory, argv[3]);
+
+    if (regcomp(&reg , argv[2], REG_EXTENDED|REG_NOSUB) != 0) {
+		fprintf(stderr,"erro regcomp\n");
+		exit(1);
+	}
+
+    strcpy(word_search, argv[2]);
 
     arq_dir *content_file = (arq_dir*)malloc(malloc_max*sizeof(arq_dir)); 
     if(content_file == NULL){
         printf("ERRO ao ALOCAR content_file\n");
         exit(1);   
     }
-
 	pthread_mutex_init(&mutex,NULL);
-
     int respost=0; 
     if((respost=list_directory(directory, content_file, word_search, number_thread)) == -1){
         printf("Erro durante a abertura dos diretorios ou Diretorio invalido\n\n");
         exit(1);
     }
-    //Volto porem ainda tem coisa dentro
-    
     if (content_file->position != 0){
-        // Chamo a funcao que cria thread
         int response = cria_thread(content_file, number_thread);
         if (response == -1){
             printf("DEU MERDA\n\n");
             exit(1);
         }
-        // Volto
-        // Printo (isso tem que ser uma secao critica)
         pthread_mutex_lock(&mutex);
         for(int h = 0; h < content_file->position; h++){
             if ((content_file+h)->lane[0] != -1){
-                printf("%s\n", (content_file+h)->dir_name);
                 int k=0;
                 while ((content_file+h)->lane[k] != -1){
-                    printf("Number lane: %d\n", (content_file+h)->lane[k]);
+                    printf("%s:", (content_file+h)->dir_name);
+                    printf("%d\n", (content_file+h)->lane[k]);
                     k++;
                     if ((content_file+h)->lane[k] == 0){
                         break;
                     }
                 }
-                printf("Type: %d\n", (content_file+h)->dir_type);
-                printf("%d\n\n", h);   
             }
         }
         content_file->position = 0;
         pthread_mutex_unlock(&mutex);
     }
 
-    // pthread_exit(NULL);
     free(content_file);
 }
